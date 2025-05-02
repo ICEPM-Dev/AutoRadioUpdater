c74e37379b297070297e39625b0433c4ff8e4e05
macasteglione <matiascasteglione2002@gmail.com>
2025-05-02 12:46:28 -0300

    fix de borrarArchivosViejos

--- README.md
+++ README.md
@@ -6,7 +6,16 @@ Primero debes crear el entorno virtual con el siguiente comando:
 python -m venv .venv
 
 Para activarlo escribe:
+```sh
 .\.venv\Scripts\activate
+```
 
 Por ultimo, para instalar las dependencias:
+```sh
 pip install -r .\requirements.txt
+```
+
+Para compilar el codigo en .exe, ejecuta el siguiente comando:
+```sh
+pyinstaller --onefile 'main.py'
+```
--- src/borrarArchivosViejos.py
+++ src/borrarArchivosViejos.py
@@ -2,17 +2,19 @@ import os
 from datetime import datetime
 
 
-def borrar_archivos_viejos(file_dir):
+def borrar_archivos_viejos(file_dir, dias_antiguedad=30):
     if not os.path.exists(file_dir):
         print(f"El directorio {file_dir} no existe.")
         return
 
-    for entry in os.scandir(file_dir):
-        if entry.is_file():
+    for root, _, files in os.walk(file_dir):
+        for name in files:
+            path = os.path.join(root, name)
             try:
-                file_date = datetime.fromtimestamp(entry.stat().st_ctime)
-                if file_date.month < datetime.today().month:
-                    print(f"Eliminando: {entry.path}")
-                    os.remove(entry.path)
+                fecha_archivo = (datetime.now() - datetime.fromtimestamp(os.stat(path).st_ctime)).days
+                print(fecha_archivo)
+                if fecha_archivo > dias_antiguedad:
+                    print(f"Eliminando: {path}")
+                    os.remove(path)
             except Exception as e:
-                print(f"Error con {entry.path}: {e}")
+                print(f"Error con {path}: {e}")
